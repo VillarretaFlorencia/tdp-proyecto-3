fe817f4119a60b80924ad459d90f5f0a2e1d85eb
ItsMassi <90329965+ItsMassi@users.noreply.github.com>
2022-11-09 22:02:37 -0300

    GUI parte 2

--- Planta vs  Zombies/src/GUI/Gameplay.java
+++ Planta vs  Zombies/src/GUI/Gameplay.java
@@ -3,13 +3,18 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JButton;
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicButtonListener;
 
-public class Gameplay extends JPanel implements ActionListener{
+
+public class Gameplay extends JLayeredPane implements ActionListener{
 	private JButton btnRestart;
+	private PanelJardin panel;
+	private JButton btnPlanta1;
+	private JButton btnPlanta2;
+	private JButton btnPlanta3;
+	private JButton btnPlanta4;
+	private int seleccion; // esta es la seleccion de la planta que quiere plantar
 	public Gameplay() {
 		JLabel lblGame = new JLabel("");
 		lblGame.setBounds(0, 0, 600, 341);
@@ -22,11 +27,39 @@ public class Gameplay extends JPanel implements ActionListener{
 		lblGame.setVisible(true);
 		setVisible(true);
 		setLayout(null);
-		add(lblGame);
+		add(lblGame,Integer.valueOf(0));
 		
 		btnRestart = new JButton("Restart");
 		btnRestart.setBounds(481, 0, 89, 23);
-		add(btnRestart);
+		add(btnRestart,Integer.valueOf(1));
+		
+		panel = new PanelJardin();
+		panel.setBounds(23, 48, 526, 293);
+		add(panel,Integer.valueOf(1));
+		
+		btnPlanta1 = new JButton("Planta1");
+		btnPlanta1.setBounds(64, 14, 57, 23);
+		add(btnPlanta1,Integer.valueOf(1));
+		btnPlanta1.addActionListener(this);
+		
+		btnPlanta2 = new JButton("Planta2");
+		btnPlanta2.setBounds(131, 14, 57, 23);
+		add(btnPlanta2,Integer.valueOf(1));
+		btnPlanta2.addActionListener(this);
+		
+		btnPlanta3 = new JButton("Planta3");
+		btnPlanta3.setBounds(192, 14, 57, 23);
+		add(btnPlanta3,Integer.valueOf(1));
+		btnPlanta3.addActionListener(this);
+		
+		btnPlanta4 = new JButton("Planta4");
+		btnPlanta4.setBounds(258, 14, 57, 23);
+		add(btnPlanta4,Integer.valueOf(1));
+		btnPlanta4.addActionListener(this);
+		
+		
+		
+		
 	}
 	
 	public JButton getRestartButton() {
@@ -35,7 +68,24 @@ public class Gameplay extends JPanel implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if(e.getSource()==btnPlanta1) {
+			System.out.println("CREANDO PLANTA 1");
+			seleccion = 1;
+		}
 		
+		if(e.getSource()==btnPlanta2) {
+			System.out.println("CREANDO PLANTA 2");
+			seleccion = 2;
+		}
+		
+		if(e.getSource()==btnPlanta3) {
+			System.out.println("CREANDO PLANTA 3");
+			seleccion = 3;
+		}
+		
+		if(e.getSource()==btnPlanta4) {
+			System.out.println("CREANDO PLANTA 4");
+			seleccion = 4;
+		}
 	}
 }
